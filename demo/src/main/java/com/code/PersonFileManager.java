b5bc81ea31f9f4b22a160016a6a224db5d0309b7
BrianPorras <brianporras6019@gmail.com>
2026-01-21 13:09:03 -0600

    Practica Guiada 2

--- demo/src/main/java/com/code/PersonFileManager.java
+++ demo/src/main/java/com/code/PersonFileManager.java
@@ -13,7 +13,7 @@ import com.google.gson.reflect.TypeToken;
 public class PersonFileManager {
 
     // Según tu estructura: src/data/person.json
-    private static final String PATH = "C:\\Users\\Brian\\Desktop\\Maven\\MavenPracticaGuiada3/person.json";
+    private static final String PATH = "C:\\Users\\Brian\\Desktop\\Maven\\MavenPracticaGuiada2/person.json";
 
     // Gson con formato bonito para el archivo
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
